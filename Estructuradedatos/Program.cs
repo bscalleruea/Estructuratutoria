57c4f43bb46cfefedb2315338aa2f6379552c43c
bscalleruea <bs.caller@uea.edu.ec>
2025-02-12 18:29:43 -0500

    Definición de Conjuntos

--- Estructuradedatos/Program.cs
+++ Estructuradedatos/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Universidad Estatal Amazonica");
+﻿using System.Transactions;
+
+Console.WriteLine("Universidad Estatal Amazonica");
+System.Console.WriteLine();
+System.Console.WriteLine("Conjuntos");
+HashSet<int> números = new HashSet<int>();
