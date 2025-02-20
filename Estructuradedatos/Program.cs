8e98d59437e8e002f30026697a0e5032d0e5ef1e
bscalleruea <bs.caller@uea.edu.ec>
2025-02-20 08:17:27 -0500

    Operaciones

--- Estructuradedatos/Program.cs
+++ Estructuradedatos/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Transactions;
-
-Console.WriteLine("Universidad Estatal Amazonica");
-System.Console.WriteLine();
+﻿Console.WriteLine("Universidad Estatal Amazonica");
+Console.WriteLine();
 System.Console.WriteLine("Conjuntos");
 HashSet<int> números = new HashSet<int>();
